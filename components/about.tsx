24a53dd5232113d796f489611cb745be23fa101b
SistaGopalaKrishna <gopalakrishnasista@gmail.com>
2024-05-21 00:44:42 +0530

    first

---
+++ components/about.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+  I am {" "}
+  <span className="font-medium">Computer Science and Engineering student</span>{" "}
+  at <span className="font-medium">NIT Andhra Pradesh</span>, currently in my
+  fourth year. I have a strong foundation in various programming languages and
+  web technologies, including <span className="font-medium">Java, Python, C,
+  C++, HTML, CSS,</span> and <span className="font-medium">JavaScript</span>,
+  as well as advanced topics like <span className="font-medium">machine
+  learning, data structures, algorithms,</span> and{" "}
+  <span className="font-medium">database management</span>. I have
+  demonstrated a keen interest in research through my work on graph theory
+  algorithms during an internship at NIT Warangal, and enjoy applying
+  theoretical concepts to practical problems. My projects, such as developing
+  a <span className="font-medium">Phishing URL Detection system, Genetic and PSO algorithms for secure domination problem, a Classroom Management System,</span> and a{" "}
+  <span className="font-medium">multiplayer chess game</span>, showcase my
+  ability to learn and adapt quickly, solve complex problems, and create
+  innovative solutions using frameworks like{" "}
+  <span className="font-medium">React, Node.js,</span> and{" "}
+  <span className="font-medium">Express.js</span>.
+</p>
+
+
+      <p>
+        <span className="italic">In my free time</span>, I enjoy making posters, playing
+        video games, watching movies. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">history</span>.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,123 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import securedominationImg from "@/public/graph-algorithms1.png";
+import phishingImg from "@/public/phishing.png";
+import classroomImg from "@/public/classmanage.png";
+import chessImg from "@/public/chesssup.png";
+
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Memeber",
+    location: "CSEA NIT Andhra Pradesh",
+    description:
+      "I word as designer and designed near 50+ posters for events and workshops. I also upskilled in Ui/Ux design.",
+    icon: React.createElement(CgWorkAlt),
+    date: "AUG 2022 - AUG 2023",
+  },
+  {
+    title: "Research Intern",
+    location: "NIT Warangal",
+    description:
+      "During the internship, I proposed a genetic algorithm-based solution for solving a well-known variant of the domination problem called secure domination problem. Presented the GENETIC ALGORITHM FOR SECURE DOMINATION PROBLEM paper in ICAMM_2023 Conference (IIT Indore) and got accepted.",
+    icon: React.createElement(CgWorkAlt),
+    date: "JUN 2023 - AUG 2023",
+  },
+  {
+    title: "Executive Member",
+    location: "Graphic Cafe NIT Andhra Pradesh",
+    description:
+      "I working as desinger. I familiar with Adobe Photoshop, Adobe Illustrator, Adobe XD, Figma, Canva",
+      icon: React.createElement(CgWorkAlt),
+    date: "SEP 2023 - present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Genetic and PSO algorithms for secure domination problem",
+    description:
+      "This project identify the optimal solution for secure domination problem using Genetic and PSO algorithms.",
+    tags: ["Python", "Algorithms", "Graphs"],
+    imageUrl: securedominationImg,
+  },
+  {
+    title: "Phishing URL Detection",
+    description:
+      "This project identify and block fraudulent website links that are designed to trick users into revealing sensitive information.",
+    tags: ["Python", "HTML", "CSS", "Pandas", "NumPy", "Machine Learning"],
+    imageUrl: phishingImg,
+  },
+  {
+    title: "Classroom Management System",
+    description:
+      "This project is a web app for managing classrooms, students, teachers, and courses. It has features like attendance, marks, reminder system and timetable.",
+    tags: ["JavaScript", "React", "Node.js", "MongoDB", "Express"],
+    imageUrl: classroomImg,
+  },
+  {
+    title: "Chesssup",
+    description:
+      "It is a web app for playing chess online with friends. It has features like real-time chat, game history, and game analysis.",
+    tags: ["React", "Redux", "JavaScript", "MongoDB", "Node.js", "Socket.io", "Express"],
+    imageUrl: chessImg,
+  },
+] as const;
+
+export const skillsData = [
+  "Java",
+  "Python",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Node.js",
+  "Git",
+  "GitHub",
+  "Express",
+  "MongoDB",
+  "Redux",
+  "MySQL",
+  "C",
+  "C++",
+  "NumPy",
+  "Pandas",
+  "DSA",
+  "Applied Machine Learning",
+  "NLP",
+  "Algorithms",
+  "Artificial Intelligence",
+  "Operating Systems",
+  "Computer Networks",
+  "Software Engineering",
+  "Linux",
+] as const;
